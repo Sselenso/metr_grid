b08d763fa6d29b97b33de3aaeda12f46a4a54bc2
Sselenso <122260649+Sselenso@users.noreply.github.com>
2023-05-20 12:36:32 +0300

    20/05_12:36

--- assets/js/burger.js
+++ assets/js/burger.js
@@ -1,8 +1,20 @@
-// JS код для бургер-меню
 const burgerBtn = document.querySelector('.header__burger');
 const burgerMenu = document.querySelector('.burger__open');
-// При клике на кнопку, добавляем/удаляем классы для открытия/закрытия бургер-меню
+const body = document.querySelector('body');
+const exitIcon = document.querySelector('.burger__exit-icon');
+
+function closeBurger() {
+  burgerBtn.classList.remove('active');
+  burgerMenu.classList.remove('active');
+  body.classList.remove('body-fixed');
+}
+
 burgerBtn.addEventListener('click', function() {
-burgerBtn.classList.toggle('active');
-burgerMenu.classList.toggle('active');
+  burgerBtn.classList.toggle('active');
+  burgerMenu.classList.toggle('active');
+  body.classList.toggle('body-fixed');
+});
+
+exitIcon.addEventListener('click', function() {
+  closeBurger();
 });
--- assets/js/form.js
+++ assets/js/form.js
@@ -10,3 +10,13 @@ function closeForm() {
 
 const closeIcon = document.querySelector('.close-icon');
 closeIcon.addEventListener('click', closeForm);
+
+
+// JS код для бургер-меню
+// const burgerBtn = document.querySelector('.header__burger');
+// const burgerMenu = document.querySelector('.burger__open');
+// При клике на кнопку, добавляем/удаляем классы для открытия/закрытия бургер-меню
+// burgerBtn.addEventListener('click', function() {
+// burgerBtn.classList.toggle('active');
+// burgerMenu.classList.toggle('active');
+// });
